85894ba1dd86006426002ee291a56b18ca140bc3
akalmykov <alexey.kalmykoff@gmail.com>
2024-10-23 18:43:51 +0300

    Initial commit

---
+++ test/siwe-verify-service.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SiweVerifyService from '../lib/siwe-verify-service-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/siwe-verify-service-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SiweVerifyService.SiweVerifyServiceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
